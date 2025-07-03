d7aaabe9f9187b41254909323bfb30c2fba18251
sungmini.kim <clever1501@naver.com>
2025-07-03 15:43:30 +0900

    [refactoring] test fixture add

--- Project24/main.cpp
+++ Project24/main.cpp
@@ -10,10 +10,14 @@ public:
 	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
 };
 
-TEST(DeviceDriver, ReadFromHW) {
-	// TODO : replace hardware with a Test Double
+class DeviceDriverFixture : public Test {
+public:
 	MockFlashMemoryDevice hardware;
 	DeviceDriver driver{ &hardware };
+
+};
+
+TEST_F(DeviceDriverFixture, ReadFromHW) {
 
 	EXPECT_CALL(hardware, read(0xFF))
 		.WillRepeatedly(Return(0));
@@ -22,42 +26,29 @@ TEST(DeviceDriver, ReadFromHW) {
 	EXPECT_EQ(0, data);
 }
 
-TEST(DeviceDriver, WriteToHW) {
-	// TODO : replace hardware with a Test Double
-	MockFlashMemoryDevice hardware;
-	DeviceDriver driver{ &hardware };
+TEST_F(DeviceDriverFixture, WriteToHW) {
 	EXPECT_CALL(hardware, write(0xFF, 0xAB))
 		.Times(1);
 	driver.write(0xFF, 0xAB);
 }
 
-TEST(DeviceDriver, ReadFromHW5TimesJustCall) {
-	// TODO : replace hardware with a Test Double
-	MockFlashMemoryDevice hardware;
-	DeviceDriver driver{ &hardware };
+TEST_F(DeviceDriverFixture, ReadFromHW5TimesJustCall) {
 	EXPECT_CALL(hardware, read(0xFF))
 		.Times(5);
 
 	driver.read(0xFF);
 }
 
-TEST(DeviceDriver, ReadFromHW5TimesSameReturn) {
-	// TODO : replace hardware with a Test Double
-	MockFlashMemoryDevice hardware;
-	DeviceDriver driver{ &hardware };
+TEST_F(DeviceDriverFixture, ReadFromHW5TimesSameReturn) {
 	EXPECT_CALL(hardware, read(0xFF))
 		.Times(5)
 		.WillRepeatedly(Return(41));
 
 	int ret = driver.read(0xFF);
-
 	EXPECT_EQ(41, ret);
 }
 
-TEST(DeviceDriver, ReadFromHW5TimesException) {
-	// TODO : replace hardware with a Test Double
-	MockFlashMemoryDevice hardware;
-	DeviceDriver driver{ &hardware };
+TEST_F(DeviceDriverFixture, ReadFromHW5TimesException) {
 	EXPECT_CALL(hardware, read(0xFF))
 		.Times(5)
 		.WillOnce(Return(41))
