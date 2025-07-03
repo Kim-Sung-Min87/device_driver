cfb03d5ccd1ca86ec2ffa62f074cab189c3cdae2
sungmini.kim <clever1501@naver.com>
2025-07-03 15:18:53 +0900

    [feature] make MockFlashMemoryDevice

--- Project24/main.cpp
+++ Project24/main.cpp
@@ -1,10 +1,22 @@
 #include "gmock/gmock.h"
 #include "device_driver.h"
 
+using namespace testing;
+
+class MockFlashMemoryDevice : public FlashMemoryDevice {
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
+
 TEST(DeviceDriver, ReadFromHW) {
 	// TODO : replace hardware with a Test Double
-	FlashMemoryDevice* hardware = nullptr;
-	DeviceDriver driver{ hardware };
+	MockFlashMemoryDevice hardware;
+	DeviceDriver driver{ &hardware };
+
+	EXPECT_CALL(hardware, read(0xFF))
+		.WillRepeatedly(Return(0));
+
 	int data = driver.read(0xFF);
 	EXPECT_EQ(0, data);
 }
